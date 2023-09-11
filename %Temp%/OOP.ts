394fcba757020ec4402080a42d0957fc8a1a9fd0
God-Tech-Info <profissional.godinho@gmail.com>
2023-09-11 18:39:59 -0300

    a

---
+++ %Temp%/OOP.ts
@@ -0,0 +1,15 @@
+//Criação de Classes
+//Classes Vão em PascalCase
+//Classe é como um molde
+
+class Conta{
+    titular: string = ""
+
+}
+
+let contaDoGodinho: Conta = new Conta();
+let contaDoElton: Conta = new Conta();
+console.log(contaDoGodinho)
+console.log(contaDoElton)
+
+//Atributos são variáveis de classe
